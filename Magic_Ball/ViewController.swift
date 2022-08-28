ad0d33a01dc8715d14a2f480dea9ede2ae6a2bae
sattarovtimurbek@gmail.com <sattarovtimurbek@gmail.com>
2022-08-28 15:50:40 +0300

    Initial Commit

---
+++ Magic_Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic_Ball
+//
+//  Created by Тимур on 28.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
